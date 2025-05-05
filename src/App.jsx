db9977eb5d2163cc2ea2c83afd045f29769ddd2a
tlang67 <160622841+tlang67@users.noreply.github.com>
2025-05-05 14:01:51 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,9 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Coupi <span className="heart">♥️</span> can help you
         </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+
       </header>
     </div>
   );
